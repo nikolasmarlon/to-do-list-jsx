7fbee85d6e38751ccd37787d175b604ae8f9fb1c
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-07-24 17:57:19 -0300

    Componentes tarefas e tarefa criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './global.css'
-
+import styles from './App.module.css'
 
 import { Header } from './componentes/Header'
+import { Tarefas } from './componentes/Tarefas'
 
 function App() {
   
 
   return (
     <div>
-
         <Header />
+      <main className={styles.header}>
+        
+        <Tarefas />       
+      
+      </main>   
         
     </div>
   )
---
+++ src/componentes/Tarefas.jsx
@@ -0,0 +1,33 @@
+import styles from './Tarefas.module.css'
+
+
+export function Tarefas(){
+    return(
+        
+
+        <div className={styles.toDoList}>  
+            <div className={styles.form}>
+                <form action="">
+                    <input></input>
+                    <button>Criar <span>+</span></button>
+                </form>
+            </div>
+
+            <header>
+                <div>
+                    <h3>Tarefas criadas <span>5</span></h3>
+                </div>
+                <div>
+                    <p>Concluídas <span>2 de 5</span></p>
+                </div>
+                
+            </header>
+
+            <section>
+
+            </section>
+        </div>
+    )
+}
+
+
