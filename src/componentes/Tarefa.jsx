891013af820ebd538b153e69ffa7790432dd8d62
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-09-02 15:52:07 -0300

    texto riscado css tarefa

--- src/componentes/Tarefa.jsx
+++ src/componentes/Tarefa.jsx
@@ -6,7 +6,7 @@ import { Trash } from 'phosphor-react'
 export function Tarefa(){
     return(
         <div className={styles.contentTarefa}>
-            <img src={tarefaEmAberto}/> <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p><span><Trash size={20} /></span>
+            <img src={tarefaConcluida}/> <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p><span><Trash size={20} /></span>
         </div>
         
     )
