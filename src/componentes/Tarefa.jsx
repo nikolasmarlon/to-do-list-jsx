cf19c9ad274294301bc2acda0cf3ec3261a03fa0
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-09-15 16:10:56 -0300

    Criando Contador de tarefas

--- src/componentes/Tarefa.jsx
+++ src/componentes/Tarefa.jsx
@@ -1,15 +1,30 @@
 import styles from './Tarefa.module.css'
 import { Trash } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Tarefa({ content, deletarTarefa}){
+
+    const [checked, setChecked] = useState(true)
+
+    function isChecked () {
+        setChecked(!checked)
+        console.log(checked)
+    }
+
+
+    const [contadorDeTarefasConluida, setContadorDeTarefasConcluida] = useState(0)
+
+    
 
     function deletarTarefa () {
         console.log(`Deletar Tarefa ${content}`)
     }
 
+    
+
     return(
         <label for={ content } className={styles.contentTarefa}>
-            <input name="task" id={ content } type="checkbox" />
+            <input onChange={isChecked} name="task" id={ content } type="checkbox" />
             <span></span>
             <p className={styles.checked}>{ content }</p>
             
